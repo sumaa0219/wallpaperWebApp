3b003a4d3d4f30a86ccef42c66dec0159a72c22b
sumaa0219 <sumaa0219@gmail.com>
2024-07-07 16:04:58 +0900

    設定完了

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Permissions-Policy',
+                        value: 'run-ad-auction=(), private-state-token-redemption=(), private-state-token-issuance=(), join-ad-interest-group=(), browsing-topics=()',
+                    },
+                ],
+            },
+        ];
+    },
+};
 
 export default nextConfig;
